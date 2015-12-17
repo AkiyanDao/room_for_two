39463c833559fb86441130663bf8dfec04cfbece
亀井章展 <Akiyan0330@kameiakiraten-no-MacBook-Pro.local>
2015-12-17 16:26:38 +0900

    更新しました

---
+++ RoomForTwo/Assets/Scripts/ball.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			Destroy (this.gameObject);
+		}
+	} 
+}
